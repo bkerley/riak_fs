04d463e4d43d105b41289cffe5c0c9157346afce
Bryce Kerley <bkerley@brycekerley.net>
2012-02-02 22:17:20 -0500

    implement contents and file?

--- lib/riakfs/filesystem.rb
+++ lib/riakfs/filesystem.rb
@@ -9,5 +9,15 @@ module RiakFS
 			dir = Directory.new @riak_client, path
 			dir.contents
 		end
+
+		def file?(path)
+			f = file_for_path(path)
+			f.exists?
+		end
+
+		private
+		def file_for_path(path)
+			File.new @riak_client, path
+		end
 	end
 end
